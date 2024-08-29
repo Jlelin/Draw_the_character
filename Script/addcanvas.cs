6778a63339515b9e7c51ff74bf0f80b3d7668c34
Jean Mendonça <107419243+Jlelin@users.noreply.github.com>
2024-08-28 21:32:33 -0300

    Add files via upload

--- Script/addcanvas.cs
+++ Script/addcanvas.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class addcanvas : NetworkBehaviour
 {
     public GameObject canvas;
-    private GameObject instanciacanvas;
+    [SerializeField] private GameObject instanciacanvas;
+    private GameObject[] jogadores;
     private NetworkObject canvasnetwork;
     private static bool foiinstanciado;
     // Start is called before the first frame update
@@ -29,33 +30,70 @@ public class addcanvas : NetworkBehaviour
         }
         else if(!foiinstanciado)
         {
-            instanciacanvas = Instantiate(canvas, this.transform);
-            canvasnetwork = instanciacanvas.GetComponent<NetworkObject>();
-            canvasnetwork.Spawn();
-            canvasnetwork.transform.SetParent(this.transform, false);
-            foiinstanciado = true;
+            jogadores = GameObject.FindGameObjectsWithTag("Player");
+            foreach(GameObject jogadorlocal in jogadores)
+            {
+                if(jogadorlocal.GetComponent<NetworkObject>().OwnerClientId == NetworkManager.Singleton.LocalClientId)
+                {
+                    instanciacanvas = Instantiate(canvas, jogadorlocal.transform);
+                    canvasnetwork = instanciacanvas.GetComponent<NetworkObject>();
+                    canvasnetwork.Spawn();
+                    canvasnetwork.transform.SetParent(jogadorlocal.transform, false);
+                    foiinstanciado = true;
+                    break;
+                }
+            }
         }
-        var botaodesenho = canvasnetwork.transform.Find("botaodesenho").gameObject;
-        var botaodesenhoscript = botaodesenho.GetComponent<botaodesenho>();
-        var charactercamera = this.transform.Find("character_camera");
-        var charactercamerascript = charactercamera.GetComponent<foco_function>();
-        charactercamerascript.botaodesenho = botaodesenho;
-        var warrior = canvasnetwork.transform.Find("warrior").gameObject;
-        charactercamerascript.warriorobject = warrior;
-        var left = canvasnetwork.transform.Find("Select Left").gameObject;
-        var guerreiroesquerdo = left.GetComponent<qual_guerreiro>();
-        charactercamerascript.guerreiroesquerdo = guerreiroesquerdo;
-        botaodesenhoscript.foco = charactercamerascript;
+        jogadores = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject jogadorlocal in jogadores)
+        {
+            if(jogadorlocal.GetComponent<NetworkObject>().OwnerClientId == NetworkManager.Singleton.LocalClientId)
+            {
+                if(IsClient)
+                {
+                    StartCoroutine(aguardandocanvas(jogadorlocal));
+                    canvasnetwork = instanciacanvas.GetComponent<NetworkObject>();
+                }
+                var botaodesenho = canvasnetwork.transform.Find("botaodesenho").gameObject;
+                var botaodesenhoscript = botaodesenho.GetComponent<botaodesenho>();
+                var charactercamera = jogadorlocal.transform.Find("character_camera");
+                var charactercamerascript = charactercamera.GetComponent<foco_function>();
+                charactercamerascript.botaodesenho = botaodesenho;
+                var warrior = canvasnetwork.transform.Find("warrior").gameObject;
+                charactercamerascript.warriorobject = warrior;
+                var left = canvasnetwork.transform.Find("Select Left").gameObject;
+                var guerreiroesquerdo = left.GetComponent<qual_guerreiro>();
+                charactercamerascript.guerreiroesquerdo = guerreiroesquerdo;
+                botaodesenhoscript.foco = charactercamerascript;
+                break;
+            }
+        }
+    }
+
+    private IEnumerator aguardandocanvas(GameObject jogadorlocal)
+    {
+        while(instanciacanvas == null)
+        {
+            instanciacanvas = jogadorlocal.transform.Find("Canvas(Clone)")?.gameObject;
+            yield return null;
+        }
     }
 
     [ServerRpc]
     private void instanciarcanvasServerRpc(ServerRpcParams serverrpcparams = default)
     {
         var clientId = serverrpcparams.Receive.SenderClientId;
-        instanciacanvas = Instantiate(canvas, this.transform);
-        canvasnetwork = instanciacanvas.GetComponent<NetworkObject>();
-        canvasnetwork.Spawn();
-        canvasnetwork.transform.SetParent(this.transform, false);
-        canvasnetwork.ChangeOwnership(clientId);
+        jogadores = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject jogadorlocal in jogadores)
+        {
+            if(jogadorlocal.GetComponent<NetworkObject>().OwnerClientId == clientId)
+            {
+                instanciacanvas = Instantiate(canvas, jogadorlocal.transform);
+                canvasnetwork = instanciacanvas.GetComponent<NetworkObject>();
+                canvasnetwork.Spawn();
+                canvasnetwork.transform.SetParent(jogadorlocal.transform, false);
+                canvasnetwork.ChangeOwnership(clientId);
+            }
+        }
     }
 }
